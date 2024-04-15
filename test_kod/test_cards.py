9b279d357cd374c0624fa1186a2869ca6aa0e6ef
Your Name <you@example.com>
2024-04-15 14:02:56 +0300

    остались только коменты

--- test_kod/test_cards.py
+++ test_kod/test_cards.py
@@ -1,21 +1,16 @@
 from src.masks import mask_account, mask_card
 
-
+"""
+# Проверка функции mask_card() с аннотациями типов
+"""
 # Проверка функции mask_card() с аннотациями типов
 def test_mask_card() -> str:
     assert mask_card("1234567890123456") == "1234 56** **** 3456"
     assert mask_card("12345") == "Некорректный номер карты"
-    return "Тест test_mask_card успешно пройден"
 
-
+"""
 # Проверка функции mask_account() с аннотациями типов
+"""
 def test_mask_account() -> str:
     assert mask_account("73654108430135874305") == "**4305"
     assert mask_account("123") == "Некорректный номер счета"
-    return "Тест test_mask_account успешно пройден"
-
-
-if __name__ == "__main__":
-    if all([test_mask_card() == "Тест test_mask_card успешно пройден",
-            test_mask_account() == "Тест test_mask_account успешно пройден"]):
-        print("Все тесты успешно пройдены.")
--- test_kod/test_sort.py
+++ test_kod/test_sort.py
@@ -1,41 +1,29 @@
-from datetime import datetime
-
+import pytest
 from typing import List
+from src.processing import sort_by_date, filter_by_state
 
-def sort_by_date(list_of_dicts: List[dict], order: str = "descending") -> List[dict]:
-    reverse = order == "descending"
-    return sorted(list_of_dicts, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
 
-# Проверка функции sort_by_date()
-input_data_sort = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
-]
+@pytest.fixture
+def input_data_filter() -> List[dict]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+    ]
 
-# Проверка функции filter_by_state()
-def filter_by_state(list_of_dicts: List[dict], state: str = "EXECUTED") -> List[dict]:
-    return [item for item in list_of_dicts if item.get("state") == state]
 
-input_data_filter = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
-]
+def test_filter_by_state_default(input_data_filter: List[dict]) -> None:
+    output_default = filter_by_state(input_data_filter, "EXECUTED")
+    assert output_default == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+    ]
 
-output_default = filter_by_state(input_data_filter, "EXECUTED")
-output_canceled = filter_by_state(input_data_filter, "CANCELED")
 
-assert output_default == [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
-]
-
-assert output_canceled == [
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
-]
-
-print("Все тесты пройдены успешно.")
+def test_filter_sort_by_date(input_data_filter: List[dict]) -> None:
+    output_canceled = filter_by_state(input_data_filter, "CANCELED")
+    assert output_canceled == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+    ]
