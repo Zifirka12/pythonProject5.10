64267bb6030dc0ca7da6e1637162507a7f1d738f
Ziflrka <139446425+Zifirka12@users.noreply.github.com>
2024-04-23 10:35:26 +0300

    Update test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -26,6 +26,8 @@ def test_filter_by_state_default(input_data_filter: List[dict]) -> None:
 def test_filter_sort_by_date(input_data_filter: List[dict]) -> None:
     output_canceled = sort_by_date(input_data_filter, "CANCELED")
     assert output_canceled == [
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
+        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
     ]
