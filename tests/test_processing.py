7a824cb5b620842e6441116b4d683a5e7e7a3993
Ziflrka <139446425+Zifirka12@users.noreply.github.com>
2024-04-26 06:30:41 +0300

    Update test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,7 @@
-import pytest
 from typing import List
-from src.processing import sort_by_date, filter_by_state
+
+import pytest
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.fixture
@@ -9,7 +10,7 @@ def input_data_filter() -> List[dict]:
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
@@ -17,13 +18,15 @@ def test_filter_by_state_default(input_data_filter: List[dict]) -> None:
     output_default = filter_by_state(input_data_filter, "EXECUTED")
     assert output_default == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
 
 def test_filter_sort_by_date(input_data_filter: List[dict]) -> None:
     output_canceled = sort_by_date(input_data_filter, "CANCELED")
     assert output_canceled == [
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
+        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
     ]
